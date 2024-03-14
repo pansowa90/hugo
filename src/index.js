c5505dfb9ad9fc36b4defb2c2c6a5701826c9926
pansowa90 <b.tomaszewski@icloud.com>
2024-03-14 23:40:26 +0100

    init hugo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // JS Goes here - ES6 supported
 
 import "./css/main.scss";
+import { gsap } from "gsap";
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+gsap.to(".box", { rotation: 45, x: 100, duration: 1 });
